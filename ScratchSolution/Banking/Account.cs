a34c5adf95ed77d0e0697bae6a1b2a70b3e2d8f5
Lloyd Boone <lboone@progressive.com>
2023-05-01 12:34:17 -0400

    Added bank account tests

---
+++ ScratchSolution/Banking/Account.cs
@@ -0,0 +1,15 @@
+﻿namespace Banking;
+
+public class Account
+{
+    private decimal _balance = 5000M;
+    public void Deposit(decimal amountToDeposit)
+    {
+        _balance += amountToDeposit;
+    }
+
+    public decimal GetBalance()
+    {
+        return _balance;
+    }
+}
---
+++ ScratchSolution/ScratchTests/BankAccount/NewAccount.cs
@@ -0,0 +1,20 @@
+﻿using Banking;
+using System.Security.Principal;
+
+namespace ScratchTests.BankAccount;
+
+public class NewAccount
+{
+    [Fact]
+    public void NewAccountsHaveCorrectBalance()
+    {
+        // Given
+        var account = new Account();
+        
+        // When
+        decimal openingBalance = account.GetBalance();
+
+        // Then
+        Assert.Equal(5000M, openingBalance);
+    }
+}
